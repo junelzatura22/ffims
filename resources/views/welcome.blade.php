164ed01f19b98fe7875da205b7a2033dd97d5569
junelzatura22 <juneldelacuadra2@gmail.com>
2023-06-30 10:50:30 +0800

    first and completed commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        Content
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  
